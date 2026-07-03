195213b82637459f00cef07368c7aa58e32b7ead
ankitsavita64 <ankitsavita0051@gmail.com>
2026-07-03 09:28:23 +0530

    update some file for docker deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-echo "======================================="
-echo "🚀 Starting SecureAuth Deployment..."
-echo "======================================="
+# echo "======================================="
+echo " Starting SecureAuth Deployment..."
+# echo "======================================="
 
 cd /home/ubuntu/secure-authhentication-sysytem
 
-echo "📥 Pulling latest code..."
-git pull origin main
-
-echo "🐳 Building and starting containers..."
+echo "Rebuilding containers..."
 docker compose up -d --build
 
-echo "🧹 Cleaning unused Docker images..."
+echo "Cleaning unused Docker images..."
 docker image prune -f
 
-echo "📋 Running containers:"
+echo "Running containers:"
 docker ps
 
-echo "======================================="
-echo "✅ Deployment Successful!"
-echo "======================================="
+echo " Deployment Successful!"
 
 '''
 #!/bin/bash
